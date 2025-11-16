c7ba3c2e0995b503b9e56f14a10179b290769453
mark <mark@marks-MacBook-Air.local>
2025-11-15 22:16:29 -0700

    working on test case

--- test/src/example.cpp
+++ test/src/example.cpp
@@ -7,13 +7,17 @@ int main()
     using namespace glfw_wrapper;
     bool init_success = init_glfw(std::cout);
 
-    if (init_success) 
-    {
-        auto window = Window::make_window(500, 300, false, true, "test");
+    ASSERT(init_success);
+
+    auto window = Window::make_window(500, 300, false, true, "test");
+
+    ASSERT(window.impl());
+
+/*
         while (!window.should_close()) {
             poll_events();
         }
-    }
+*/ 
 
     return 0;
 }
